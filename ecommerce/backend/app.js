aedffd1aaff86e939e1a68da2b7df2a2fa4fc484
Vibhor Singh <vibhor.formal@gmail.com>
2021-04-11 10:32:33 +0530

    Controllers added

--- ecommerce/backend/app.js
+++ ecommerce/backend/app.js
@@ -1,4 +1,26 @@
 const express = require('express')
 const app= express();
+require('dotenv').config()
+const mongoose = require('mongoose');
+// load env variables
+ 
+//routes
+const userRoutes=require('./routes/user');
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
 
-module.exports=app
+
+
+app.use("/api",userRoutes);
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
